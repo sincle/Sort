f8f043cda3bdc8b6533834f091460f563a366a3b
kang <agudao@sina.com>
2018-04-26 09:34:46 +0800

    插入,希尔

--- app/src/main/java/com/haieros/sort/sort/Sort.java
+++ app/src/main/java/com/haieros/sort/sort/Sort.java
@@ -21,6 +21,7 @@ public class Sort {
      */
     public static void bubbleSort(int[] data){
         long start = System.currentTimeMillis();
+        int temp;
         //开始
         //最后一次不需要比较
         for (int i = 0; i < data.length-1; i++) {
@@ -29,7 +30,7 @@ public class Sort {
                 //比较2个元素
                 if(data[j] > data[j+1]) {
                     //交换
-                    int temp = data[j];
+                    temp = data[j];
                     data[j] = data[j+1];
                     data[j+1] = temp;
                 }
@@ -38,7 +39,8 @@ public class Sort {
         //结束
         long end = System.currentTimeMillis();
         String arrString = Arrays.toString(data);
-        System.out.println("冒泡排序: 时间:"+(end-start)+"ms,result: "+arrString);
+        System.out.println("冒泡排序: 时间:"+(end-start)+"ms");
+        System.out.println("reuslt:"+arrString);
     }
 
     /**
@@ -51,7 +53,7 @@ public class Sort {
         long start = System.currentTimeMillis();
         int temp, minIndex;
         //开始
-        //假设data[0] 最小,然后寻找最小值替换它 剩下最后一个不需要比较
+        //假设data[i] 最小,然后寻找最小值替换它 剩下最后一个不需要比较
         for (int i = 0; i < data.length - 1; i++) {
             minIndex = i; //假设是最小值
             for (int j = i+1; j < data.length - 1; j++) {
@@ -67,6 +69,110 @@ public class Sort {
         //结束
         long end = System.currentTimeMillis();
         String arrString = Arrays.toString(data);
-        System.out.println("选择排序: 时间:"+(end-start)+"ms,result: "+arrString);
+        System.out.println("选择排序: 时间:"+(end-start)+"ms");
+        System.out.println("reuslt:"+arrString);
+    }
+
+    /**
+     * 插入排序（Insertion-Sort）的算法描述是一种简单直观的排序算法。它的工作原理是通过构建有序序列，
+     * 对于未排序数据，在已排序序列中从后向前扫描，找到相应位置并插入。
+     * @param data
+     */
+    public static void insertSort(int[] data){
+        long start = System.currentTimeMillis();
+        // 目标元素
+        int target;
+        //目标元素前面的元素
+        int preIndex;
+        //开始
+        //假设第一个元素是最小值 从 i = 1 处开始
+        for (int i = 1; i < data.length; i++) {
+            //保存目标元素
+            target = data[i];
+            //记录目标元素index
+            preIndex = i;
+            //如果满足 目标元素 < 其前一个元素  则覆盖之
+            //i 之前的元素 已经排序完成 故 只需要data[preIndex - 1] > target
+            while (preIndex > 0 && data[preIndex - 1] > target){
+                //使用前一个元素覆盖当前元素
+                data[preIndex] = data[preIndex - 1];
+                // 自减 指向preIndex 前一个元素
+                preIndex--;
+            }
+            //使用目标元素覆盖之
+            data[preIndex] = target;
+        }
+        //结束
+        long end = System.currentTimeMillis();
+        String arrString = Arrays.toString(data);
+        System.out.println("插入排序: 时间:"+(end-start)+"ms");
+        System.out.println("reuslt:"+arrString);
+    }
+
+    /**
+     * 第一个突破O(n2)的排序算法，是简单插入排序的改进版。它与插入排序的不同之处在于，它会优先比较距离较
+     * 远的元素。希尔排序又叫缩小增量排序。
+     *
+     * @param data
+     */
+    public static void shellSort(int[] data){
+        long start = System.currentTimeMillis();
+        //开始
+        int len = data.length;
+        // 间隔
+        int h = 1;
+        //前元素索引
+        int preIndex;
+        //目标元素
+        int target;
+        //步长选取
+        while (h <= len / 3) {
+            h = h * 3 + 1;
+        }
+        //步长判断
+        while (h > 0) {
+            //步长间隔内,index 变化
+            for (int i = 0; i < h; i++) {
+                //以 h 为间隔的所有元素集合 插入排序
+                for (int j = h + i; j < len; j = j + h) {
+                    //前一个元素
+                    preIndex = j - h;
+                    //目标元素
+                    target = data[j];
+                    //之前的元素 大于目标元素 用目标元素 覆盖之
+                    while (preIndex >= 0 && target < data[preIndex]){
+                        //覆盖之
+                        data[preIndex+h] = data[preIndex];
+                        //取preIndex 前一个元素
+                        preIndex = preIndex - h;
+                    }
+                    data[preIndex + h] = target;
+                }
+            }
+            //步长变化
+            h = (h - 1) / 3;
+        }
+        //结束
+        long end = System.currentTimeMillis();
+        String arrString = Arrays.toString(data);
+        System.out.println("希尔排序: 时间:"+(end-start)+"ms");
+        System.out.println("reuslt:"+arrString);
+    }
+
+    /**
+     * 归并排序 (merge sort) 是一类与插入排序、交换排序、选择排序不同的另一种排序方法。归并的含义是将两个
+     * 或两个以上的有序表合并成一个新的有序表。归并排序有多路归并排序、两路归并排序 , 可用于内排序，也可
+     * 以用于外排序。
+     * @param data
+     */
+    public static void mergeSort(int[] data){
+        long start = System.currentTimeMillis();
+        //开始
+
+        //结束
+        long end = System.currentTimeMillis();
+        String arrString = Arrays.toString(data);
+        System.out.println("希尔排序: 时间:"+(end-start)+"ms");
+        System.out.println("reuslt:"+arrString);
     }
 }
